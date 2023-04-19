baf5daf26465e614eb265626644af6651dd73cbb
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-04-19 10:13:16 +0300

    Static script - 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-# installs nginx if absent
+# installs nginx if absent and sets up directory from which to serve web files
 if [ ! -x "$(command -v nginx)" ]; then
     sudo apt-get -y update
     sudo apt-get -y install nginx
 fi
 # data directory
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 # change ownership
-sudo chown -R "$USER":"$USER" /data/
+chown -R "$USER":"$USER" /data/
 echo -e "<html>\n <head>\n  <title>Test</title>\n <head>\n <body>\n  <p>Holberton School</p>\n </body>\n</html>" > /data/web_static/releases/test/index.html
 # remove link
 if [ -L "/data/web_static/current" ]; then
     rm -rf /data/web_static/current
 fi
 ln -s /data/web_static/releases/test/ /data/web_static/current
-# sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
