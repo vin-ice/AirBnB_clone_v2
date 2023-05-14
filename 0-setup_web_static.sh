ebac967546d00c07d1f4ba75048eb5e319525b5a
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-05-14 22:35:23 +0300

    0-stat

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 # installs nginx if absent and sets up directory from which to serve web files
-if [ ! -x "$(command -v nginx)" ]; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
-# data directory
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-# change ownership
-sudo chown -R "$USER":"$USER" /data/
-echo -e "<html>\n <head>\n  <title>Test</title>\n <head>\n <body>\n  <p>Holberton School</p>\n </body>\n</html>" > /data/web_static/releases/test/index.html
-# remove link
-if [ -L "/data/web_static/current" ]; then
-    rm -rf /data/web_static/current
-fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
 exit 0
